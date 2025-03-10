5056041d121fa2da9809f28878c18c744b11dedb
courtneymahugu <courtneymahugu@gmail.com>
2025-03-10 01:01:20 -0400

    Initial Commit

---
+++ Project 2 BeReal Clone PT 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2 BeReal Clone PT 1
+//
+//  Created by Courtney Mahugu on 3/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
